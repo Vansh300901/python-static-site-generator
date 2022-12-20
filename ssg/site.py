21be450f7e126987768e99fe301099238b867eae
Vansh300901 <98076578+Vansh300901@users.noreply.github.com>
2022-12-19 16:36:51 -0800

    The Site Class
    Added the parser function

--- ssg/site.py
+++ ssg/site.py
@@ -19,7 +19,14 @@ class Site:
     def load_parser(self, extension):
         for parser in self.parsers:
             if(parser.valid_extension(extension)):
-                return parserf
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        
+
+
+
 
 
 
