d72e0c42e7cd23d3ffb98e54b8e76a89952b61b1
Vansh300901 <98076578+Vansh300901@users.noreply.github.com>
2022-12-19 16:17:13 -0800

    The Parser Class
    Modified the write method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -17,3 +17,7 @@ class Parser:
     def write(self, path, source, dest, ext = ".html"):
         full_path = dest/path.with_suffix(ext).name
 
+        with open(full_path, "w") as file:
+            file.write()
+        
+
